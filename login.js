a2ae8768db015a830bd00b49c9a9bceb210494e1
vivianefrts <115565161+vivianefrts@users.noreply.github.com>
2022-12-05 15:11:34 -0300

    primeiro commit
    página de Login e Recuperação de senha

---
+++ login.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputEmail = e.target.querySelector('#email');
+    const inputSenha = e.target.querySelector('#senha');
+
+    const email = inputEmail.value;
+    const senha = inputSenha.value;
+
+    if (email !== Login.email || senha !== Login.senha) {
+        setResultado("E-mail ou senha inválida", false);
+        
+    } else {const msg = `Login realizado com sucesso`;
+    setResultado(msg, true);
+    window.location.href = '../../index.html';
+    }
+});
